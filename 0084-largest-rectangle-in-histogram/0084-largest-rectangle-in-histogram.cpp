1ff679b2eeaa379fde9395441353b6dc74925f54
Samarth Sharma <itisisamarth@gmail.com>
2026-08-08 17:13:48 +0530

    Time: 15 ms (85.89%), Space: 81.5 MB (61.56%) - LeetHub

--- 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
+++ 0084-largest-rectangle-in-histogram/0084-largest-rectangle-in-histogram.cpp
@@ -2,60 +2,103 @@ class Solution {
 public:
     int largestRectangleArea(vector<int>& heights) {
 
-        int n = heights.size();
+        // int n = heights.size();
         
+        // stack<int>st;
+
+        // // Next Smallest Left
+
+        // vector<int>nextSmallLeft(n);
+        // for(int i=n-1;i>=0;i--)
+        // {
+        //     while(!st.empty() && heights[st.top()] > heights[i])
+        //     {
+        //         nextSmallLeft[st.top()] = i;
+        //         st.pop();
+        //     }
+        //     st.push(i);
+        // };
+
+        // // Might Stack contains some element-->
+        // while(!st.empty())
+        // {
+        //     nextSmallLeft[st.top()] = -1;
+        //     st.pop();
+        // }
+
+        // // Next Smallest Right
+
+        // vector<int>nextSmallRight(n);
+        // for(int i=0;i<n;i++)
+        // {
+        //     while(!st.empty() && heights[st.top()] > heights[i])
+        //     {
+        //         nextSmallRight[st.top()] = i;
+        //         st.pop();
+        //     }
+        //     st.push(i);
+        // };
+
+        // // Might Stack contains some element-->
+        // while(!st.empty())
+        // {
+        //     nextSmallRight[st.top()] = n;
+        //     st.pop();
+        // }
+
+        // // Calculate Area -->
+
+        // int ans = 0;
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     ans = max(ans, heights[i] * (nextSmallRight[i] - nextSmallLeft[i] - 1));
+        // }
+
+        // return ans;
+
+        // In Single Pass -->
+
+        int n = heights.size();
         stack<int>st;
-
-        // Next Smallest Left
-
-        vector<int>nextSmallLeft(n);
-        for(int i=n-1;i>=0;i--)
-        {
-            while(!st.empty() && heights[st.top()] > heights[i])
-            {
-                nextSmallLeft[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        };
-
-        // Might Stack contains some element-->
-        while(!st.empty())
-        {
-            nextSmallLeft[st.top()] = -1;
-            st.pop();
-        }
-
-        // Next Smallest Right
-
-        vector<int>nextSmallRight(n);
-        for(int i=0;i<n;i++)
-        {
-            while(!st.empty() && heights[st.top()] > heights[i])
-            {
-                nextSmallRight[st.top()] = i;
-                st.pop();
-            }
-            st.push(i);
-        };
-
-        // Might Stack contains some element-->
-        while(!st.empty())
-        {
-            nextSmallRight[st.top()] = n;
-            st.pop();
-        }
-
-        // Calculate Area -->
-
         int ans = 0;
+        int idx = 0;
 
         for(int i=0;i<n;i++)
         {
-            ans = max(ans, heights[i] * (nextSmallRight[i] - nextSmallLeft[i] - 1));
+            while(!st.empty() && heights[st.top()] > heights[i])
+            {
+                idx = st.top();
+                st.pop();
+
+                if(!st.empty())
+                {
+                    ans = max(ans, heights[idx]*(i - st.top() - 1));
+                }
+                else
+                {
+                    ans = max(ans, heights[idx] * i);
+                }
+            }
+            st.push(i);
         }
 
-        return ans;
+        while(!st.empty())
+            {
+                idx = st.top();
+                st.pop();
+
+                if(!st.empty())
+                {
+                    ans = max(ans, heights[idx]*(n - st.top() - 1));
+                }
+                else
+                {
+                    ans = max(ans, heights[idx] * n);
+                }
+            }
+
+            return ans;
         
     }
 };
