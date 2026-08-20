3b7697c4ab04a0cfc97c36a7a5dc987e1f85d9f4
Samarth Sharma <itisisamarth@gmail.com>
2026-08-20 20:51:47 +0530

    Time: 0 ms (100%), Space: 10.1 MB (19.19%) - LeetHub

---
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -0,0 +1,105 @@
+class MyStack {
+public:
+    queue<int>q1;
+    queue<int>q2;
+
+    MyStack() {
+        
+    }
+    
+    void push(int x) {
+        // Both q1 q2 empty
+
+        if(empty())
+        q1.push(x);
+
+        // Jis Queue me Element hoga usme push
+        else if(q1.empty())
+        q2.push(x);
+
+        else
+        q1.push(x);
+    }
+    
+    int pop() {
+
+        if(empty())
+        return 0;
+
+        else if(q1.empty())
+        {
+            while(q2.size() > 1)
+            {
+                q1.push(q2.front());
+                q2.pop();
+            }
+
+            int ele = q2.front();
+            q2.pop();
+            return ele;
+        }
+
+        else
+        {
+            while(q1.size() > 1)
+            {
+                q2.push(q1.front());
+                q1.pop();
+            }
+
+            int ele = q1.front();
+            q1.pop();
+            return ele;
+        }
+
+    }
+    
+    int top() {
+        // Stack is empty -->
+        if(q1.empty())
+        {
+        while(q2.size() > 1)
+        {
+            q1.push(q2.front());
+            q2.pop();
+        }
+
+        int ele = q2.front();
+
+        q2.pop();
+        q1.push(ele);
+
+        return ele;
+    }
+
+    else
+    {
+        while(q1.size() > 1)
+        {
+            q2.push(q1.front());
+            q1.pop();
+        }
+
+        int ele = q1.front();
+
+        q1.pop();
+        q2.push(ele);
+
+        return ele;
+    }
+    
+    }
+    
+    bool empty() {
+        return q1.empty() && q2.empty();
+    }
+};
+
+/**
+ * Your MyStack object will be instantiated and called as such:
+ * MyStack* obj = new MyStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * bool param_4 = obj->empty();
+ */
